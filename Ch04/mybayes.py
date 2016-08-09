ba35c48be41e827814b14b7cfd6e6876fa4f538c
clubjack <clubjack213@gmail.com>
2016-08-09 20:36:18 +0800

    ham下23.txt读取有问题，做了修改，完成了邮件分类测试

--- Ch04/mybayes.py
+++ Ch04/mybayes.py
@@ -12,6 +12,9 @@ P(c|x)=P(x|c)*P(c)/P(x)
 此处使用分类器对文本进行分类
 """
 from pylab import *
+import random
+
+
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                  ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -35,6 +38,7 @@ def setOfWords2Vec(vocabList,inputSet):
             print('the word: %s is not in my vocabulary!' % word)
     return returnVec
 def trainNB0(trainMatrix,trainCategory):
+    #trainMatrix和trainCategory类型是array
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory)/numTrainDocs
@@ -58,6 +62,7 @@ def trainNB0(trainMatrix,trainCategory):
     p0Vect = log(p0Num/p0Denom)
     return p0Vect,p1Vect,pAbusive
 def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    #vec2Classify类型为array
     p0 = sum(vec2Classify*p0Vec)+log(1-pClass1)
     p1 = sum(vec2Classify*p1Vec)+log(pClass1)
     return 1 if p1>p0 else 0
@@ -74,3 +79,50 @@ def testingNB():
     testEntry = ['stupid','garbage']
     thisDoc = array(setOfWords2Vec(myVocabList,testEntry))
     print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+    
+#以下，使用分类器对email目录下的邮件进行分类
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'\W',bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+def spamTest():
+    docList=[];classList = [];fullText=[]
+    #以下，读取email目录下spam和ham两个子目录里的文本文件
+    #并解析成字符串列表
+    #每篇文档的字符串列表保存在docList中，对应的分类保存在classList中
+    for i in range(1,26):
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+#        print(i)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    #生成完整字符串表
+    vocabList = createVocabList(docList)
+    #50表示两个子目录中共有50个文件
+    trainingSet = list(range(50)); testSet = []
+    #选取10个作为测试集，剩下的作为训练集
+    for i in range(10):
+        randIndex = random.randint(0,len(trainingSet)-1)
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat = [];trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList,docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    #得到训练结果
+    p0V,p1V,pAb = trainNB0(array(trainMat),array(trainClasses))
+    
+    errorCount = 0
+    for docIndex in testSet:
+        wordVec = setOfWords2Vec(vocabList,docList[docIndex])
+        result = classifyNB(array(wordVec),p0V,p1V,pAb)
+        if result != classList[docIndex]:
+            errorCount+=1
+            print('error classify:',docList[docIndex],docIndex)
+    print('the error rate is: ',errorCount/len(testSet))
+    
+        
