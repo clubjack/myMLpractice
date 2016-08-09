50a98bce7fdc40cb930a8b8ce180868c332f651d
clubjack <clubjack213@gmail.com>
2016-08-09 14:10:59 +0800

    第四章贝叶斯分类器初步完成

--- Ch04/mybayes.py
+++ Ch04/mybayes.py
@@ -11,7 +11,7 @@ P(c|x)=P(x|c)*P(c)/P(x)
 无法实现精确分类的情况下，返回概率最大的分类结果
 此处使用分类器对文本进行分类
 """
-
+from pylab import *
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                  ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -34,5 +34,43 @@ def setOfWords2Vec(vocabList,inputSet):
         else:
             print('the word: %s is not in my vocabulary!' % word)
     return returnVec
-    
-    
+def trainNB0(trainMatrix,trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory)/numTrainDocs
+    p0Num = ones(numWords)
+    p1Num = ones(numWords)
+    p0Denom = 2
+    p1Denom = 2
+    #以下为计算训练矩阵中每个单词的条件概率P(Wi|Ci)
+    #即文档中每个单词在对应文档类别中出现的概率
+    #计算方式为遍历所有文档单词，如果属于某类别，
+    #则该单词量加1，该类别单词量加1
+    #最后每个单体的数量除以该类别总单词量得到每个单词的条件概率
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = log(p1Num/p1Denom)
+    p0Vect = log(p0Num/p0Denom)
+    return p0Vect,p1Vect,pAbusive
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p0 = sum(vec2Classify*p0Vec)+log(1-pClass1)
+    p1 = sum(vec2Classify*p1Vec)+log(pClass1)
+    return 1 if p1>p0 else 0
+def testingNB():
+    listOfPosts,listClasses = loadDataSet()
+    myVocabList = createVocabList(listOfPosts)
+    trainMat = []
+    for postinDoc in listOfPosts:
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    p0V,p1V,pAb = trainNB0(trainMat,listClasses)
+    testEntry = ['love','my','dalmation']
+    thisDoc = array(setOfWords2Vec(myVocabList,testEntry))
+    print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+    testEntry = ['stupid','garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList,testEntry))
+    print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
